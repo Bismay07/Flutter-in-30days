29c2c48174344c37e755a35bc3998386c88930df
Bismay Bibhu Prakash <bismayssp1540@gmail.com>
2023-02-28 12:04:58 +0530

    completed day 8. validation and change of login button changed after the backing to previous page.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,89 +10,105 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool isclicked = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if(_formKey.currentState!.validate()) {
+      setState(() {
+        isclicked = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        isclicked = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/Mobile_login.png"),
-          Text(
-            "Welcome $name",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Color.fromRGBO(109, 98, 255, 1),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Image.asset("assets/images/Mobile_login.png"),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Color.fromRGBO(109, 98, 255, 1),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter your username", labelText: "Username"),
-                  onChanged: (value) {
-                    name = value;
-                    setState(() {});
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter your Password",
-                    labelText: "Password",
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  // child: ElevatedButton(
-                  //     style: ElevatedButton.styleFrom(
-                  //         backgroundColor: Color.fromRGBO(109, 98, 255, 1)),
-                  //     onPressed: () {
-                  //       Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  //     },
-                  //     child: Container(
-                  //         height: 40,
-                  //         width: 150,
-                  //         child: Center(
-                  //             child: Text(
-                  //           "Login",
-                  //           style: TextStyle(fontSize: 18),
-                  //         )))),
-                  child: InkWell(
-                    onTap: () async {
-                      setState(() {
-                        isclicked = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your username", labelText: "Username"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Username cannot be empty";
+                      }
+                      return null;
                     },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      height: isclicked ? 50 :40,
-                      width: isclicked ? 50 : 150,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(109, 98, 255, 1),
-                          borderRadius:  BorderRadius.circular(isclicked ? 50 : 7.7),
-
-                      ),
-                      child: isclicked ? Icon(Icons.done, color: Colors.white,) : Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter your Password",
+                      labelText: "Password",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Password cannot be empty";
+                      }
+                      if (value!.length < 6) {
+                        return "Password length must be atleast 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Material(
+                      color: Color.fromRGBO(109, 98, 255, 1),
+                      borderRadius: BorderRadius.circular(isclicked ? 50 : 7.7),
+                      child: InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          height: isclicked ? 50 : 40,
+                          width: isclicked ? 50 : 150,
+                          alignment: Alignment.center,
+                          child: isclicked
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
